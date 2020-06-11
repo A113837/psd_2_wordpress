58d53aee17b9bf56452ab4bef84e73f15c6d1de1
A113837 <padi196@live.com>
2020-06-12 05:23:38 +0800

    Add files via upload

---
+++ 02 PHP The Basics -  Up And Running with PHP/027 PHP Functions - Functions with arguments/027_function_parameter.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Introduction to PHP</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,900&amp;subset=latin-ext" rel="stylesheet">
+    <style type="text/css">
+      * {
+        box-sizing: border-box;
+      }
+      html {
+        height: 100%;
+      }
+      body {
+        min-height: 100%;
+        margin:0;
+        background-color: #00c4ff;
+        box-sizing: border-box;
+        font-family: 'Lato', sans-serif;
+      }
+      div.container {
+        max-width: 1100px;
+        margin: 0 auto;
+        width: 90%;
+        background-color: white;
+        height: 100vh;
+        padding: 30px;
+      }
+      h1 {
+        font-size: 30px;
+        text-align: center;
+      }
+      div.code-content {
+        background-color: #e1e1e1;
+        padding:30px;
+        margin-top: 40px;
+      }
+
+    </style>
+  </head>
+  <body>
+    
+    <div class="container">
+      <h1>Up & Running with PHP</h1>
+      
+
+      <div class="code-content">
+      <?php 
+        
+        function say_hello($name)
+        {
+          echo "hello $name<br>";
+        }
+
+        //call the function
+        say_hello('John');
+        say_hello('Mike');
+
+        function sum($a, $b)
+        {
+          echo $a+$b ."<br>";
+        }
+
+        sum(4,7);
+
+       ?>
+      </div>
+    </div>
+    
+  </body>
+</html>
