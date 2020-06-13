2d213bfbfeb4528009e7446fe09a0b3c8fdd3bcc
A113837 <padi196@live.com>
2020-06-13 15:46:28 +0800

    Add files via upload

---
+++ 03 Our First Project - Restaurant WordPress Theme/041 Printing the menu in our theme/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>La Pizzeria</title>
+	<?php wp_head(); ?>
+</head>
+<body>
+	<header class="site-header">
+		<div class="logo">
+			<a href="<?php echo esc_url(home_url('/') ); ?>">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" class="logoimage">
+			</a>
+			
+		</div><!-- logo -->
+	</header>
+
+	<div class="navigation">
+		<?php 
+			$args = array(
+				'theme_location' 	=> 'header-menu',
+				'container'			=>	'nav',	
+				'container_class'	=>	'site-nav'
+			);
+			wp_nav_menu($args);
+		 ?>
+	</div><!-- navigatio -->
+
