2f3a9512869f23f75eabd37499b705854ed28a36
A113837 <padi196@live.com>
2020-06-13 16:06:08 +0800

    Add files via upload

---
+++ 03 Our First Project - Restaurant WordPress Theme/042 Creating a Social Menu/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>La Pizzeria</title>
+	<?php wp_head(); ?>
+</head>
+<body>
+	<header class="site-header">
+		<div class="logo">
+			<a href="<?php echo esc_url(home_url('/') ); ?>">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" class="logoimage">
+			</a>
+			
+		</div><!-- logo -->
+		<div class="header-information">
+			<div class="socials">
+				
+			</div><!-- socials -->
+			<div class="address">
+				<p>B179 Bay Avenue Mountain View CA</p>
+				<p>Phone Number: +1-453-435-9879</p>
+			</div><!-- address -->
+		</div><!-- header-information -->
+	</header>
+
+	<div class="navigation">
+		<?php 
+			$args = array(
+				'theme_location' 	=> 'header-menu',
+				'container'			=>	'nav',	
+				'container_class'	=>	'site-nav'
+			);
+			wp_nav_menu($args);
+		 ?>
+	</div><!-- navigatio -->
+
