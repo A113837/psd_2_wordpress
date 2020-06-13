7f569976a844f4bf6f7a512fa245618fee389392
A113837 <padi196@live.com>
2020-06-13 12:31:59 +0800

    Add files via upload

---
+++ 03 Our First Project - Restaurant WordPress Theme/039 Adding the Logo into the header.php/functions.php
@@ -0,0 +1,21 @@
+<?php 
+
+function lapizzeria_styles()
+{
+
+	//adding normalize.css
+	wp_register_style('normalize', get_template_directory_uri().'/css/normalize.css', array(), '8.0.0' );
+
+	//adding stylesheet
+	wp_register_style('style', get_template_directory_uri().'/style.css', array('normalize'), '1.0.0' );
+	
+	//enqueue style
+	wp_enqueue_style('normalize');
+	wp_enqueue_style('style');
+}
+
+//hook using add_action
+
+add_action('wp_enqueue_scripts', 'lapizzeria_styles');
+
+ ?>
---
+++ 03 Our First Project - Restaurant WordPress Theme/039 Adding the Logo into the header.php/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>La Pizzeria</title>
+	<?php wp_head(); ?>
+</head>
+<body>
+	<header class="site-header">
+		<div class="logo">
+			<a href="<?php echo esc_url(home_url('/') ); ?>">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" class="logoimage">
+			</a>
+			
+		</div><!-- logo -->
+	</header>
+
