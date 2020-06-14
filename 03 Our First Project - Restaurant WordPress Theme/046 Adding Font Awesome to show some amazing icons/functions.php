5382b05abed89f1b0d633d0e5a1247be363eef4d
A113837 <padi196@live.com>
2020-06-14 10:03:10 +0800

    Add files via upload

---
+++ 03 Our First Project - Restaurant WordPress Theme/046 Adding Font Awesome to show some amazing icons/functions.php
@@ -0,0 +1,36 @@
+<?php 
+
+function lapizzeria_styles()
+{
+
+	//adding normalize.css
+	wp_register_style('normalize', get_template_directory_uri().'/css/normalize.css', array(), '8.0.0' );
+
+	//adding font-awesome.css
+	wp_register_style('fontawesome', get_template_directory_uri().'/css/font-awesome.css', array(), '4.7.0' );
+
+	//adding stylesheet
+	wp_register_style('style', get_template_directory_uri().'/style.css', array('normalize'), '1.0.0' );
+	
+	//enqueue style
+	wp_enqueue_style('normalize');
+	wp_enqueue_style('fontawesome');
+	wp_enqueue_style('style');
+}
+
+//hook using add_action
+
+add_action('wp_enqueue_scripts', 'lapizzeria_styles');
+
+//add Menus
+function lapizzeria_menu()
+{
+	register_nav_menus(array(
+		'header-menu' => __('Header Menu', 'lapizzeria'),
+		'social-menu' => __('Social Menu', 'lapizzeria')
+	));
+}
+//'init is a hook in wp'
+add_action('init', 'lapizzeria_menu');
+
+ ?>
