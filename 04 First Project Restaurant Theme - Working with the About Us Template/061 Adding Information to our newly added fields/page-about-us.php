dbbf484a28731f8d13cc5e660ec392e6a7e42510
A113837 <padi196@live.com>
2020-06-17 13:00:21 +0800

    Add files via upload

---
+++ 04 First Project Restaurant Theme - Working with the About Us Template/061 Adding Information to our newly added fields/page-about-us.php
@@ -0,0 +1,32 @@
+<?php get_header();?>
+
+	<?php while(have_posts()): the_post(); ?>
+		<div class="hero" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);">
+			<div class="hero-content">
+				<div class="hero-text">
+					<h2><?php the_title(); ?></h2>
+					<h4>Hello from page-about-us.php</h4>
+				</div><!-- hero-text -->
+			</div><!-- hero-content -->
+		</div><!-- hero -->
+
+		<div class="main-content container">
+			<main class="text-center content-text">
+				<?php the_content(); ?>
+			</main>
+		</div><!-- main-conten -->	
+
+		<div class="box-information container clear">
+			<div class="box">
+				<div class="content-box">
+					<?php the_field('description_1'); ?>
+				</div><!-- content-box -->
+			</div><!-- box -->
+			
+		</div><!-- box-information container clear -->
+
+	<?php endwhile; ?>
+	
+
+
+<?php get_footer();	?>
